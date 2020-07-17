fd74a32f3d1330d340e110119869dccd86593b69
JurekMateusz <56219248+JurekMateusz@users.noreply.github.com>
2020-07-17 13:14:48 +0200

    Change Bootstrap version to 4 and new look.

---
+++ src/main/java/pl/mjurek/notepage/controller/user/VerificationAccountController.java
@@ -0,0 +1,29 @@
+package pl.mjurek.notepage.controller.user;
+
+import pl.mjurek.notepage.exception.UpdateObjectException;
+import pl.mjurek.notepage.service.AccountActionService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/verification")
+public class VerificationAccountController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String destination = "WEB-INF/index.jsp";
+
+        String key = request.getParameter("key");
+        AccountActionService service = new AccountActionService();
+        try {
+            service.verification(key);
+        } catch (UpdateObjectException e) {
+            destination = "WEB-INF/error.jsp";
+        }
+        request.setAttribute("successMessage","Verification successful");
+        request.getRequestDispatcher(destination).forward(request, response);
+    }
+}
